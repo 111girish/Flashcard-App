dccc10e3d6460f38780969c9d07ed6a6809c3bd6
111girish <pokhrelgirish94@gmail.com>
2026-05-16 18:55:38 +0545

    Trying to handle this bitchass POOL

--- db.js
+++ db.js
@@ -1,1 +1,15 @@
-// This module is used to connect database to this bitchass project...
+import { Pool } from 'pg';
+import getEnv from './config.js';
+
+const params = getEnv('dbConnect');
+
+const pool = new Pool({
+  connectionString: params,
+  max: 20, // set pool max size to 20
+  idleTimeoutMillis: 1000, // close idle clients after 1 second
+  connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+  maxUses: 7500, // close (and replace) a connection after it has been used 7500 times 
+})
+
+export default pool;
+
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import getEnv from './config.js';
+import pool from './db.js';
 
 import apiRoutes from './routes/api.js';
-
-
-
 
 const app = express();
 const PORT = getEnv('port');
@@ -15,6 +13,16 @@ app.use(bodyParser.json());
 app.use('/api', apiRoutes);
 
 app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
+
+(async () => {
+  const client = await pool.connect()
+  try {
+    const result = await client.query('select * from user_info')
+    console.log('hello from', result.rows[0])
+  } finally {
+    client.release()
+  }
+})().catch((e) => console.error(e.message, e.stack))
 
 app.get('/', (req, res) => res.send('HELLO FROM HOMEPAGE!'));
 
