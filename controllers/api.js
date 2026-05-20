c4e5506b6e7b7b3d2d07e704f3d803ad00d44d84
111girish <pokhrelgirish94@gmail.com>
2026-05-20 20:46:54 +0545

    Connecting to postgresql

--- controllers/api.js
+++ controllers/api.js
@@ -2,7 +2,8 @@ import bcrypt from "bcrypt";
 import pool from "../db.js";
 
 export const register = async (req, res) => {
-  const { first_name, last_name, password, username, email, phone_no } = req.body;
+  const { first_name, last_name, password, username, email, phone_no } =
+    req.body;
 
   if (!first_name || !last_name || !password || !username || !email) {
     res.send(`Something is missing man`);
@@ -13,22 +14,38 @@ export const register = async (req, res) => {
   //hashes the fucking password using bcrypt;
   const hashed = await bcrypt.hash(password, saltRounds);
 
-  const text = `INSERT INTO users(first_name, last_name, password, username, email, phone_no) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`;
+  const text = `INSERT INTO users(first_name, last_name, password, username, email, phone_no) VALUES($1, $2, $3, $4, $5, $6) RETURNING *;`;
   const values = [first_name, last_name, hashed, username, email, phone_no];
 
   const client = await pool.connect();
   try {
     const result = await client.query(text, values);
     console.log("Data saved:", result.rows[0]);
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
     console.error("Error executing query.");
-    res.status(500).json({message: "User registration failed"})
+    res.status(500).json({ message: "User registration failed" });
   } finally {
     client.release();
   }
 };
 
-export const login =  (req, res) => {
+export const login = async (req, res) => {
+  const { first_name, email, username } = req.body;
+  if (!first_name || !username || !email) {
+    res.send(`Something is missing man`);
+    return;
+  }
   
-}
+  const text = "SELECT * FROM users WHERE username = $1;";
+  const values = username;
+
+  const client = pool.connect();
+  try{
+    const result = await client.query(text, values);
+  } 
+  catch(err) {
+    console.error("There seems to be a fucking problem nilla;")
+  }
+
+};
