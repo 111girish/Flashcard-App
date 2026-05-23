8c37f41f1221d3e79bd9cafba719d1f07f9d6247
111girish <pokhrelgirish94@gmail.com>
2026-05-23 23:57:41 +0545

    Making card.js controller right now. You can use req.params.id to get deck id

---
+++ controllers/card.js
@@ -0,0 +1,14 @@
+export const cardCreate = async (req, res) => {
+
+  const userId = req.user.userId;
+  const deckId = req.params.id;
+
+  
+
+
+
+}
+
+export const cardGet = async (req, res) =>{
+
+}
