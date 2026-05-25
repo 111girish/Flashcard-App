79b8f4fe4a18fab285b7e2c5ddfde1c454049539
111girish <pokhrelgirish94@gmail.com>
2026-05-26 03:57:00 +0545

    Finished creating register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,36 +1,78 @@
 import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     first_name: "",
-    last_name:"",
+    last_name: "",
     username: "",
-    email:"",
-    password: ""
-  })
+    email: "",
+    password: "",
+  });
+  // const [error, setError] = useState("");
 
-  const handleSubmit = () => {
-    console.log(formData.first_name);
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        formData,
+      );
+      console.log("User added!!!", response.data);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
     setFormData({
-      ...formData, 
-      [name]: value
-    })
-  }
+      ...formData,
+      [name]: value,
+    });
+  };
 
   return (
     <>
       <h1>Register</h1>
-      <form onSubmit = {handleSubmit}>
-        <input type="text" placeholder="first name" onChange = {handleChange} />
-        <input type="text" placeholder="last name" onChange = {handleChange}/>
-        <input type="text" placeholder="username" onChange = {handleChange}/>
-        <input type="email" placeholder="email" onChange = {handleChange}/>
-        <input type="text" placeholder="password" onChange = {handleChange}/>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="first name"
+          name="first_name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="last name"
+          name="last_name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
         <input type="submit"></input>
       </form>
     </>
