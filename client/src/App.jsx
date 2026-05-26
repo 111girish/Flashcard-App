bd9756330e09ee18a2eb40622cc937887d44e640
111girish <pokhrelgirish94@gmail.com>
2026-05-26 21:13:01 +0545

    Created Dashboard.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Login from "./pages/login";
 import Register from "./pages/Register";
 import Homepage from "./pages/Homepage";
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
