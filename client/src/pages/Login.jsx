20f1a74c7d6ac0dd723344304230a2bdf1b34add
111girish <pokhrelgirish94@gmail.com>
2026-05-26 20:57:07 +0545

    Created Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,48 @@
-const Login = async () => {
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+
+  const navigate = useNavigate();
+
+  const [loginData, setLoginData] = useState({
+    username: "",
+    email: "", 
+    password: ""
+  })
+  
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setLoginData({
+      ...loginData,
+      [name] : value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(loginData);
+
+    try {
+      const result = await axios.post("http://localhost:5000/api/auth/login/", loginData)
+      console.log("User is qualified", result.data);
+      localStorage.setItem('token', result.data.token);
+      navigate("/dashboard");
+    }
+    catch(err){
+      console.log(err);
+    };
+
+  }
 
 
   return (
     <>
-      <form >
-        <input placeholder="username" name='username' />
-        <input placeholder='email' name='email' />
-        <input placeholder='password' name='password' />
+      <form onSubmit={handleSubmit}>
+        <input placeholder="username" name='username' onChange={handleChange} />
+        <input placeholder='email' name='email' onChange={handleChange}/>
+        <input placeholder='password' name='password' onChange={handleChange} />
         <input type="submit" />
       </form>
     </>
