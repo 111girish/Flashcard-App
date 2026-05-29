ee513e17c0c1235de340820d0310081f6ce711fc
111girish <pokhrelgirish94@gmail.com>
2026-05-29 18:02:02 +0545

    Finished creating card.jsx all by myself

--- client/src/pages/Card.jsx
+++ client/src/pages/Card.jsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 const Card = () => {
   const [cards, setCards] = useState([]);
   const { deckId } = useParams();
+  const [text, setText] = useState({ front_text: "", back_text: "" });
 
   useEffect(() => {
     const cardGet = async () => {
@@ -19,15 +20,68 @@ const Card = () => {
     cardGet();
   }, []);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setText({ ...text, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const cardPost = async () => {
+      const { front_text, back_text } = text;
+      const token = localStorage.getItem("token");
+      const result = await axios.post(
+        `http://localhost:5000/api/decks/${deckId}/cards`,
+        { front_text, back_text, deckId },
+        { headers: { Authorization: `Bearer ${token}` } },
+      );
+      const data = result.data.data;
+      setCards([...cards, data]);
+    };
+    cardPost();
+  };
+
+  const handleDelete = async (cardId) => {
+    const token = localStorage.getItem("token");
+    await axios.delete(`http://localhost:5000/api/cards/${cardId}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    cards.filter((card) => card.card_id !== cardId);
+  };
+
   return (
     <>
       <h1>This is a fucking Card</h1>
       {!cards.length && <h3>There are no cards here!!</h3>}
-      {cards.length>0 && (<ul>
-        {cards.map((card) => {
-          <li key={card.card_id}>{card.front_text} === {card.back_text}</li>
-        })}
-      </ul>)}
+      {cards.length > 0 && (
+        <ul>
+          {cards.map((card) => {
+            <li key={card.card_id}>
+              {card.front_text} === {card.back_text}
+              <button
+                onClick={() => {
+                  handleDelete(card.card_id);
+                }}
+              >
+                Delete
+              </button>
+            </li>;
+          })}
+        </ul>
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Front text"
+          onChange={handleChange}
+          name="front_text"
+        />
+        <input
+          placeholder="Back text"
+          onChange={handleChange}
+          name="back_text"
+        />
+        <input type="submit" />
+      </form>
     </>
   );
 };
